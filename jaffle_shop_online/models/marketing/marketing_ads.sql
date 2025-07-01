3a8fffbbcc74e6c130436688b90aaaeb5697debd
Michael Myaskovsky <michael@elementary-data.com>
2025-07-01 08:34:22 +0000

    Optimize marketing_ads model for better performance and maintainability

--- jaffle_shop_online/models/marketing/marketing_ads.sql
+++ jaffle_shop_online/models/marketing/marketing_ads.sql
@@ -5,19 +5,42 @@
     )
 }}
 
+{% set columns_to_select = [
+    "ad_id",
+    "date",
+    "campaign_name",
+    "ad_group_name",
+    "ad_name",
+    "clicks",
+    "impressions",
+    "spend"
+] %}
+
 with google_ads as (
-    select *
+    select 
+        {{ columns_to_select | join(', ') }}
     from {{ source("ads", "stg_google_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 ),
 
 facebook_ads as (
-    select *
+    select 
+        {{ columns_to_select | join(', ') }}
     from {{ source("ads", "stg_facebook_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 ),
 
 instagram_ads as (
-    select *
+    select 
+        {{ columns_to_select | join(', ') }}
     from {{ source("ads", "stg_instagram_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 )
 
 select *, 'google' as utm_source
