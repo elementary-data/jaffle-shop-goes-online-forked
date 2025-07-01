b1f5c831f7ffca5c6cf224f60e6b2097a5231400
Nimrod <nimrod+demo@elementary-data.com>
2025-07-01 10:09:21 +0000

    Optimize marketing_ads model for better performance and maintainability

--- jaffle_shop_online/models/marketing/marketing_ads.sql
+++ jaffle_shop_online/models/marketing/marketing_ads.sql
@@ -5,26 +5,61 @@
     )
 }}
 
+{% set columns_to_select = [
+    "ad_id",
+    "date",
+    "campaign_name",
+    "ad_group_name",
+    "spend",
+    "impressions",
+    "clicks"
+] %}
+
 with google_ads as (
-    select *
+    select 
+        {{ columns_to_select | join(', ') }},
+        'google' as utm_source
     from {{ source("ads", "stg_google_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 ),
 
 facebook_ads as (
-    select *
+    select 
+        {{ columns_to_select | join(', ') }},
+        'facebook' as utm_source
     from {{ source("ads", "stg_facebook_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 ),
 
 instagram_ads as (
-    select *
+    select 
+        {{ columns_to_select | join(', ') }},
+        'instagram' as utm_source
     from {{ source("ads", "stg_instagram_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 )
 
-select *, 'google' as utm_source
+select 
+    *,
+    current_timestamp() as created_at
 from google_ads
+
 union all
-select *, 'facebook' as utm_source
+
+select 
+    *,
+    current_timestamp() as created_at
 from facebook_ads
+
 union all
-select *, 'instagram' as utm_source
+
+select 
+    *,
+    current_timestamp() as created_at
 from instagram_ads
