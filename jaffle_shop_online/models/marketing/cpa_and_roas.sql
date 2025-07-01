cc6f41a17d63e51749e53b38253d40032e42ef31
Mika Kerman <mika@elementary-data.com>
2025-07-01 14:28:07 +0000

    Fix cpa_and_roas model: Use dbt refs and verify calculations

--- jaffle_shop_online/models/marketing/cpa_and_roas.sql
+++ jaffle_shop_online/models/marketing/cpa_and_roas.sql
@@ -1,60 +1,38 @@
-{{
-    config(
-        materialized = "table",
-    )
-}}
 
 with ad_spend as (
-
     select * from {{ ref('ads_spend') }}
-
 ),
 
 attribution as (
-
     select * from {{ ref('attribution_touches') }}
-
 ),
 
 sessions as (
-
     select * from {{ ref('sessions') }}
-
 ),
 
 -- aggregate first as this is easier to debug / often leads to fewer fanouts
 ad_spend_aggregated as (
-
     select
         day,
         utm_source,
-
         sum(spend) as total_spend
-
     from ad_spend
-
     group by 1, 2
-
 ),
 
 attribution_aggregated as (
-
     select
         sess.started_at::date as day,
         sess.utm_source,
-
         sum(linear_points) as attribution_points,
         sum(linear_revenue) as attribution_revenue
-
     from attribution attr
     inner join sessions sess on (sess.session_id = attr.session_id and sess.customer_id = attr.customer_id)
-
     group by 1, 2
-
 ),
 
 joined as (
-
     select
         coalesce(attr_agg.day, spend_agg.day) as day,
         coalesce(attr_agg.utm_source, spend_agg.utm_source) as utm_source,
@@ -79,7 +57,6 @@ joined as (
     from attribution_aggregated attr_agg
     full outer join ad_spend_aggregated spend_agg
         on attr_agg.day = spend_agg.day and attr_agg.utm_source = spend_agg.utm_source
-
 )
 
 select * from joined
