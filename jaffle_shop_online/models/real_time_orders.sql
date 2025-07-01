dd1a4a5eaa0b231955c8db197fda4b0b4153fa15
Mika <mika+demo@elementary-data.com>
2025-07-01 09:33:25 +0000

    Fix ROAS anomaly: Normalize monetary units across historical and real-time orders

--- jaffle_shop_online/models/real_time_orders.sql
+++ jaffle_shop_online/models/real_time_orders.sql
@@ -1,23 +1,22 @@
-{{
-  config(materialized='view')
-}}
 
-{% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
+
+
 
 with orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ source('jaffle_shop', 'orders') }}
 ),
 
 payments as (
-    select * from {{ ref('stg_payments') }}
+    select * from {{ source('stripe', 'payments') }}
 ),
 
 order_payments as (
     select
         order_id,
-        {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
-        {% endfor -%}
+        sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
+        sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
+        sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
+        sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount,
         sum(amount) as total_amount
     from payments
     group by order_id
@@ -29,25 +28,27 @@ final as (
         o.customer_id,
         o.order_date,
         o.status,
-        {% for payment_method in payment_methods -%}
-        op.{{ payment_method }}_amount,
-        {% endfor -%}
+        op.credit_card_amount,
+        op.coupon_amount,
+        op.bank_transfer_amount,
+        op.gift_card_amount,
         op.total_amount    as amount_cents
     from orders o
     left join order_payments op on o.order_id = op.order_id
 )
 
+-- All monetary amounts in this model are in dollars
 select 
     order_id,
     customer_id,
     order_date,
     status,
-    {{ cents_to_dollars('amount_cents') }} as amount,
-    {{ cents_to_dollars('bank_transfer_amount') }} as bank_transfer_amount,
-    {{ cents_to_dollars('coupon_amount') }} as coupon_amount,
-    {{ cents_to_dollars('credit_card_amount') }} as credit_card_amount,
-    {{ cents_to_dollars('gift_card_amount') }} as gift_card_amount
+    (amount_cents / 100)::numeric(16, 2) as amount,
+    (bank_transfer_amount / 100)::numeric(16, 2) as bank_transfer_amount,
+    (coupon_amount / 100)::numeric(16, 2) as coupon_amount,
+    (credit_card_amount / 100)::numeric(16, 2) as credit_card_amount,
+    (gift_card_amount / 100)::numeric(16, 2) as gift_card_amount
 from final
 where date(order_date) = (
     select date(max(order_date)) from final
-) 
+)
