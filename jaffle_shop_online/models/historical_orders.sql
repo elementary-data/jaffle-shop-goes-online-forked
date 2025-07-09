421330a66d8b69bc2be0131c1a61c3cea82606ae
Maayan <maayan+172@elementary-data.com>
2025-07-09 12:01:10 +0000

    Fix unit mismatch in historical_orders model

--- jaffle_shop_online/models/historical_orders.sql
+++ jaffle_shop_online/models/historical_orders.sql
@@ -1,23 +1,21 @@
-{{
-  config(materialized='view')
-}}
 
-{% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
+
 
 with orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from ELEMENTARY_TESTS.mika_jaffle_shop_online.stg_orders
 ),
 
 payments as (
-    select * from {{ ref('stg_payments') }}
+    select * from ELEMENTARY_TESTS.mika_jaffle_shop_online.stg_payments
 ),
 
 order_payments as (
     select
         order_id,
-        {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
-        {% endfor -%}
+        sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
+        sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
+        sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
+        sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount,
         sum(amount) as total_amount
     from payments
     group by order_id
@@ -29,10 +27,11 @@ final as (
         o.customer_id,
         o.order_date,
         o.status,
-        {% for payment_method in payment_methods -%}
-        op.{{ payment_method }}_amount,
-        {% endfor -%}
-        op.total_amount    as amount
+        (op.credit_card_amount / 100)::numeric(16, 2) as credit_card_amount,
+        (op.coupon_amount / 100)::numeric(16, 2) as coupon_amount,
+        (op.bank_transfer_amount / 100)::numeric(16, 2) as bank_transfer_amount,
+        (op.gift_card_amount / 100)::numeric(16, 2) as gift_card_amount,
+        (op.total_amount / 100)::numeric(16, 2) as amount
     from orders o
     left join order_payments op on o.order_id = op.order_id
 )
@@ -42,4 +41,4 @@ from final
 where date(order_date) < (
     select date(max(order_date))
     from final
-) 
+)
