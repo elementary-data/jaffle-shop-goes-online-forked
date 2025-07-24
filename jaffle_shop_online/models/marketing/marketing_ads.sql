c0f96113356771210e79ae52bcd791a8d57739d6
Maayan <maayan+172@elementary-data.com>
2025-07-24 18:08:56 +0000

    Optimize marketing_ads model with explicit columns and incremental logic

--- jaffle_shop_online/models/marketing/marketing_ads.sql
+++ jaffle_shop_online/models/marketing/marketing_ads.sql
@@ -6,25 +6,49 @@
 }}
 
 with google_ads as (
-    select *
+    select
+        ad_id,
+        date,
+        utm_campain,
+        utm_medium,
+        cost,
+        'google' as utm_source
     from {{ source("ads", "stg_google_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 ),
 
 facebook_ads as (
-    select *
+    select
+        ad_id,
+        date,
+        utm_campain,
+        utm_medium,
+        cost,
+        'facebook' as utm_source
     from {{ source("ads", "stg_facebook_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 ),
 
 instagram_ads as (
-    select *
+    select
+        ad_id,
+        date,
+        utm_campain,
+        utm_medium,
+        cost,
+        'instagram' as utm_source
     from {{ source("ads", "stg_instagram_ads") }}
+    {% if is_incremental() %}
+    where date > (select max(date) from {{ this }})
+    {% endif %}
 )
 
-select *, 'google' as utm_source
-from google_ads
+select * from google_ads
 union all
-select *, 'facebook' as utm_source
-from facebook_ads
+select * from facebook_ads
 union all
-select *, 'instagram' as utm_source
-from instagram_ads
+select * from instagram_ads
