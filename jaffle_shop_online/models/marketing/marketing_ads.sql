142aa77664333fd99578488c2f80520248ce7d32
Maayan <maayan+172@elementary-data.com>
2026-05-28 20:14:39 +0000

    fix(marketing_ads): replace select * with explicit columns to prevent silent column-shift on union all

--- jaffle_shop_online/models/marketing/marketing_ads.sql
+++ jaffle_shop_online/models/marketing/marketing_ads.sql
@@ -6,25 +6,40 @@
 }}
 
 with google_ads as (
-    select *
+    select
+        ad_id,
+        date,
+        utm_medium,
+        utm_campain,
+        cost,
+        'google' as utm_source
     from {{ source("ads", "stg_google_ads") }}
 ),
 
 facebook_ads as (
-    select *
+    select
+        ad_id,
+        date,
+        utm_medium,
+        utm_campain,
+        cost,
+        'facebook' as utm_source
     from {{ source("ads", "stg_facebook_ads") }}
 ),
 
 instagram_ads as (
-    select *
+    select
+        ad_id,
+        date,
+        utm_medium,
+        utm_campain,
+        cost,
+        'instagram' as utm_source
     from {{ source("ads", "stg_instagram_ads") }}
 )
 
-select *, 'google' as utm_source
-from google_ads
+select * from google_ads
 union all
-select *, 'facebook' as utm_source
-from facebook_ads
+select * from facebook_ads
 union all
-select *, 'instagram' as utm_source
-from instagram_ads
+select * from instagram_ads
